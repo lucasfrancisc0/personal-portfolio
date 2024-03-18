b42cf34d81fcc442bfe5759c3419174ccadb39fd
Lucas <lucasarnoldd@gmail.com>
2024-03-18 16:06:02 -0300

    :sparkles: feat(layout): Create layout 	Create layout and add it to the routes.

--- web/src/routes/customer.routes.tsx
+++ web/src/routes/customer.routes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { DefaultLayout } from "../layout/default";
 
 export function CustomerRoutes() {
   return(
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="/" element={<Home/>}/>  
+      </Route>
     </Routes>
   )
 };
